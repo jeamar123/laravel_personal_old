bd85d6566f4dfdd0f7dc2e470ac574325ef53f53
Jeamar <jeamar@gmail.com>
2018-02-22 11:09:38 +0800

    updates

--- public/assets/expenses/process/directives/mapDirective.js
+++ public/assets/expenses/process/directives/mapDirective.js
@@ -11,43 +11,220 @@ app.directive('mapDirective', [
       {
         console.log( "mapDirective Runinng !" );
 
+        var iconMarkerBase = 'https://maps.google.com/mapfiles/kml/shapes/';
         var map;
         var infoWindow;
+        var currentPositionMarker ;
+        var myLatlng;
+        var infowindow = [];
 
-        scope.initMap = () => {
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 14
-          });
-          infoWindow = new google.maps.InfoWindow;
+        var markers = [];
+        var directionsRenderer = [];
+        var selected_notif_arr = [];
 
-          // Try HTML5 geolocation.
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-              var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-              };
+        var directionsService = new google.maps.DirectionsService();
+        var directionsMatrixservice = new google.maps.DistanceMatrixService();
 
-              infoWindow.setPosition(pos);
-              infoWindow.setContent('Location found.');
-              infoWindow.open(map);
-              map.setCenter(pos);
-            }, function() {
-              handleLocationError(true, infoWindow, map.getCenter());
-            });
-          } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
+
+        scope.active_notif = [];
+
+        scope.notif_list = [
+          {
+            id : 1,
+            latlng : {lat:8.5090004, lng:124.76753},
+            message : `Hi! I have a big mouth.`,
+            name : `Anal Alzula`,
+            img : 'allan.jpg',
+            icon : 'allanIcon.jpg',
+          },
+          {
+            id : 2,
+            latlng : {lat:8.4890626, lng:124.5964074},
+            message : `Help!`,
+            name : `Matt Batman`,
+            img : 'matt.jpg',
+            icon : 'mattIcon.jpg',
+          },
+          {
+            id: 3,
+            latlng : {lat:8.4754904, lng:124.6197734},
+            message : `Hi! I'm a Pervert`,
+            name : `Manyak Kun`,
+            img : 'billy.jpg',
+            icon : 'billyIcon.jpg',
+          },
+          {
+            id: 4,
+            latlng : {lat:8.480177, lng:124.642613},
+            message : `Hi! I'm also a Pervert`,
+            name : `Kuya ni Manyak Kun`,
+            img : 'jhon.jpg',
+            icon : 'jhonIcon.jpg',
+          },
+        ];
+
+        scope.selectNotif = ( notif ) =>{
+          // console.log(notif);
+          var checkActive = $.inArray( notif.id, scope.active_notif );
+          // console.log(checkActive);
+          if( checkActive < 0 ){
+            notif.active = true;
+            selected_notif_arr.push(notif.latlng);
+            scope.active_notif.push(notif.id);
+            
+            scope.calcRoute(notif);
+            scope.addMarker(notif);
+            scope.addInfoWindow(notif);
+            
+          }else{
+            notif.active = false;
+            markers[checkActive].setMap(null);
+            markers.splice(checkActive,1);
+            directionsRenderer[checkActive].setMap(null);
+            directionsRenderer.splice(checkActive,1);
+            selected_notif_arr.splice(checkActive,1);
+            scope.active_notif.splice(checkActive,1);
+          }
+
+          var bounds = new google.maps.LatLngBounds();
+          bounds.extend( currentPositionMarker.getPosition() );
+
+          for (var i = 0; i < selected_notif_arr.length; i++) {
+            bounds.extend(new google.maps.LatLng(selected_notif_arr[i].lat, selected_notif_arr[i].lng));
+          }
+
+          map.fitBounds(bounds);
+
+          if( selected_notif_arr.length == 0 ){
+            map.setZoom(12);
           }
         }
 
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
-          infoWindow.open(map);
+
+        //====== MAP INITIALIZATIONS =======//
+
+        scope.calcRoute = ( notif ) =>{
+
+          var request = {
+            origin: currentPositionMarker.getPosition(),
+            destination: notif.latlng,
+            travelMode: 'DRIVING'
+          };
+          directionsService.route(request, function(result, status) {
+            if (status == 'OK') {
+              console.log(result);
+              directionsRenderer[$.inArray( notif.id, scope.active_notif )] = new google.maps.DirectionsRenderer({
+                draggable: true,
+                map: map,
+                suppressMarkers: true
+              });
+
+              directionsRenderer[$.inArray( notif.id, scope.active_notif )].setMap(map);
+              directionsRenderer[$.inArray( notif.id, scope.active_notif )].setDirections(result);
+
+              directionsRenderer[$.inArray( notif.id, scope.active_notif )].addListener('directions_changed', function() {
+                computeTotalDistance(directionsRenderer[$.inArray( notif.id, scope.active_notif )].getDirections());
+              });
+            }
+          });
+
+          directionsMatrixservice.getDistanceMatrix({
+            origins: [currentPositionMarker.getPosition()],
+            destinations: [notif.latlng],
+            travelMode: 'DRIVING',
+            // transitOptions: TransitOptions,
+            // drivingOptions: DrivingOptions,
+            // unitSystem: UnitSystem,
+            // avoidHighways: Boolean,
+            // avoidTolls: Boolean,
+          }, function success(response, status) {
+            console.log(response);
+          });
+        }
+
+        scope.addMarker = ( notif ) =>{
+          var position = new google.maps.LatLng(notif.latlng.lat, notif.latlng.lng);
+          var imageIcon = {
+            url: 'assets/expenses/img/users/' + notif.icon,
+            // This marker is 20 pixels wide by 32 pixels high.
+            size: new google.maps.Size(50, 50),
+            // The origin for this image is (0, 0).
+            origin: new google.maps.Point(0, 0),
+            // The anchor for this image is the base of the flagpole at (0, 32).
+            anchor: new google.maps.Point(25, 50),
+          };
+
+          markers[ $.inArray( notif.id, scope.active_notif ) ] = new google.maps.Marker({
+            position: position,
+            map: map,
+            icon: imageIcon,
+            animation: google.maps.Animation.DROP,
+          });
+        }
+
+        scope.addInfoWindow = ( notif ) =>{
+          var contentString = notif.name + ` : ` + notif.message;
+
+          infowindow[ $.inArray( notif.id, scope.active_notif ) ] = new google.maps.InfoWindow({
+            content: contentString
+          });
+
+          markers[ $.inArray( notif.id, scope.active_notif ) ].addListener('click', function() {
+            scope.closeAllInfoWindows();
+            infowindow[$.inArray( notif.id, scope.active_notif )].open(map, markers[ $.inArray( notif.id, scope.active_notif ) ]);
+          });
+        }
+
+        scope.closeAllInfoWindows = ( ) =>{
+          for (var i = 0; i < infowindow.length; i++) {
+            infowindow[i].close();
+          }
+        }
+
+        scope.initMap = () => {
+          map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          });
+
+          var myoverlay = new google.maps.OverlayView();
+          myoverlay.draw = function () {
+            this.getPanes().markerLayer.id='markerLayer';
+          };
+          myoverlay.setMap(map);
+
+          scope.autoUpdateMap();
+        }
+
+        scope.autoUpdateMap = () => {
+          navigator.geolocation.getCurrentPosition(
+            function success(position) {  
+            console.log(position.coords);
+
+            var newPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            if (currentPositionMarker) {
+              currentPositionMarker.setPosition(newPoint);
+            }else {
+              // var newPoint = new google.maps.LatLng(8.4787481, 124.6037888);
+              currentPositionMarker = new google.maps.Marker({
+                position: newPoint,
+                map: map,
+                animation: google.maps.Animation.DROP,
+              });
+            }
+
+            map.setCenter(newPoint);
+          }, 
+          function error(err) {  
+            console.log(err);
+          },{
+            maximumAge:60000, 
+            timeout:5000, 
+            enableHighAccuracy:true
+          }); 
+
+          // setTimeout(scope.autoUpdateMap, 5000);
         }
 
         scope.onLoad = ( ) =>{
